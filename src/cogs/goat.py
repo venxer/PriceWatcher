06697f6d765a8e81f81230f4f80ea54bde965dec
Edwin Zhao <edwinzhao123@gmail.com>
2024-01-02 19:20:16 -0500

    goat single and bulk scraper

--- src/cogs/goat.py
+++ src/cogs/goat.py
@@ -1,71 +1,111 @@
 import discord
 from discord.ext import commands
 import json
+import re
 
 #for webscraping
-import requests 
 import cloudscraper
 
-from bs4 import BeautifulSoup
-
 def getInfo(productID):
-    scraper = cloudscraper.create_scraper()
-
-    URL = f"https://www.goat.com/web-api/v1/product_variants/buy_bar_data?productTemplateId={productID}&countryCode=US"
-    scraper = cloudscraper.create_scraper()
-    response = scraper.get(URL)
+    output_arr = ["", "", ""]
+    scraper = cloudscraper.create_scraper(
+        browser={
+            'browser': 'firefox',
+            'platform': 'windows',
+            'mobile': False
+        }
+    )
+    response = scraper.get(f"https://www.goat.com/web-api/v1/product_variants/buy_bar_data?productTemplateId={productID}&countryCode=US")
     result = json.loads(response.text)
 
-    return(result)
+    for entry in result:
+        if entry["boxCondition"] == "good_condition" and entry["shoeCondition"] == "new_no_defects" and entry != "":
+                
+            try:
+                size = entry["sizeOption"]["value"]
+            except KeyError:
+                size = "N/A"
+
+            try:
+                ask = str(entry["lowestPriceCents"]["amountUsdCents"])
+                askString = "$" + "{:.2f}".format(float(ask) / 100)
+            except KeyError:
+                askString = "N/A"
+
+            try:
+                lastSold = str(entry["lastSoldPriceCents"]["amountUsdCents"])
+                lastSoldString = "$" + "{:.2f}".format(float(lastSold) / 100)
+            except KeyError:
+                lastSold = "N/A"
+
+            output_arr[0] += "**" + str(size) + "**\n" 
+            output_arr[1] += askString + "\n"
+            output_arr[2] += lastSoldString +"\n"
+
+    return output_arr
 
 class goat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
-        
     @commands.command()
     async def goat(self, ctx, *, arg):
-        scraper = cloudscraper.create_scraper()
+        scraper = cloudscraper.create_scraper(
+        browser={
+            'browser': 'firefox',
+            'platform': 'windows',
+            'mobile': False
+            }
+        )
 
         URL = f"https://ac.cnstrc.com/search/{arg}?c=ciojs-client-2.35.2&key=key_XT7bjdbvjgECO5d8&i=cedf6174-1982-4a30-ad4c-bffe0e7babc0&s=1&page=1&num_results_per_page=24&sort_by=relevance&sort_order=descending&fmt_options%5Bhidden_fields%5D=gp_lowest_price_cents_3&fmt_options%5Bhidden_fields%5D=gp_instant_ship_lowest_price_cents_3&fmt_options%5Bhidden_facets%5D=gp_lowest_price_cents_3&fmt_options%5Bhidden_facets%5D=gp_instant_ship_lowest_price_cents_3&variations_map=%7B%22group_by%22%3A%5B%7B%22name%22%3A%22product_condition%22%2C%22field%22%3A%22data.product_condition%22%7D%2C%7B%22name%22%3A%22box_condition%22%2C%22field%22%3A%22data.box_condition%22%7D%5D%2C%22values%22%3A%7B%22min_regional_price%22%3A%7B%22aggregation%22%3A%22min%22%2C%22field%22%3A%22data.gp_lowest_price_cents_3%22%7D%2C%22min_regional_instant_ship_price%22%3A%7B%22aggregation%22%3A%22min%22%2C%22field%22%3A%22data.gp_instant_ship_lowest_price_cents_3%22%7D%7D%2C%22dtype%22%3A%22object%22%7D&qs=%7B%22features%22%3A%7B%22display_variations%22%3Atrue%7D%2C%22feature_variants%22%3A%7B%22display_variations%22%3A%22matched%22%7D%7D&_dt=1703990333165"
-        print(URL)
-        embedMsg = discord.Embed(title = f"Search **Goat** for **{arg}**",
-                            url=URL, color = 0x0071ce) 
         
-        # Headers ={'accept': '*/*',
-        #           'accept-encoding': 'gzip, deflate, br',
-        #           'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-        #           'dnt': '1',
-        #           'origin': 'https://www.goat.com',
-        #           'sec-fetch-dest': 'empty',
-        #           'sec-fetch-mode': 'cors',
-        #           'sec-fetch-site': 'cross-site',
-        #           'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'}
-        # response = requests.get(URL, headers=Headers)
         response = scraper.get(URL)
         result = json.loads(response.text)
 
-        # try:
-        topResult = result["response"]["results"][0]
-        productID = topResult["data"]["id"]
-        productTitle = topResult["value"]
-        productURL = "https://www.goat.com/sneakers/" + topResult["data"]["slug"]
-        
-        test = getInfo("test")
-        print(test)
-        
-        await ctx.send(productTitle)
-        await ctx.send(productURL)
-        # except:
-        await ctx.send("Product Not Found")
+        try:
+            topResult = result["response"]["results"][0]
 
-        # await ctx.send(embed = embedMsg)
-    
+            productID = topResult["data"]["id"]
+            productTitle = topResult["value"]
+            productURL = "https://www.goat.com/sneakers/" + topResult["data"]["slug"]
+            productImage = topResult["data"]["image_url"]
+            productRetail = "N/A" if topResult["data"]["retail_price_cents"] == 0 else "$" + "{:.2f}".format(float(topResult["data"]["retail_price_cents"]) / 100)
+            
+            productInfo = getInfo(productID)
+
+            embedMsg = discord.Embed(title = productTitle,
+                                     url = productURL, 
+                                     color = 0xB702FD)
+            embedMsg.set_thumbnail(url=productImage)
+            embedMsg.add_field(name= "Retail: ", value= productRetail, inline=False)
+            embedMsg.add_field(name= "Size: ", value= productInfo[0], inline=True)
+            embedMsg.add_field(name= "Lowest Ask: ", value= productInfo[1], inline=True)
+            embedMsg.add_field(name= "Last Sold: ", value= productInfo[2], inline=True)
+
+            embedMsg.set_footer(text= "Edwin Z.", icon_url= "https://www.edwinz.dev/img/profile_picture.jpg")
+
+            await ctx.send(embed = embedMsg)
+        
+        except:
+            embedMsg = discord.Embed(title = arg,
+                                     url = "https://www.goat.com/sneakers/", 
+                                     color = 0xB702FD)
+            embedMsg.add_field(name= "Product Not Found", value="" ,inline=False)
+            embedMsg.set_footer(text= "Edwin Z.", icon_url= "https://www.edwinz.dev/img/profile_picture.jpg")
+            await ctx.send(embed = embedMsg)
+
+    @commands.command()
+    async def goatb(self, ctx, *, arg):
+        print("bulk goat")
+        SKUs = re.split(r';|,|\n| |, | ,', arg)
+
+        for SKU in SKUs:
+            await self.goat(ctx, arg=SKU)
 
     #error checking
     @goat.error
-    async def building_search_error(self, ctx, error):
+    async def goat_error(self, ctx, error):
         await ctx.send(str(error))
 
 #setting up the discord bot for usage
